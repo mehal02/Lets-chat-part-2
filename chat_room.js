2f602603a429f4887cb329ea10e25c7649080b31
mehal02 <140321143+mehal02@users.noreply.github.com>
2023-10-07 15:54:51 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtHx-xY4k6m2Pv_xx4v5vwaTptH8jk97g",
+    authDomain: "lets-chat-web-app-1bbe2.firebaseapp.com",
+    projectId: "lets-chat-web-app-1bbe2",
+    storageBucket: "lets-chat-web-app-1bbe2.appspot.com",
+    messagingSenderId: "910398917663",
+    appId: "1:910398917663:web:5744c080cc5ca5d9cbd92a",
+    measurementId: "G-EGGMF59MSJ"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
